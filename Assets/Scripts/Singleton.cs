598956b57443b3012879e309b680c2481b2e33f4
Jared Mohney <jared.mohney@gmail.com>
2020-05-07 23:03:52 -0600

    add Singleton

--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -2,6 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Singleton : MonoBehaviour
+// Singleton take #1
+// Allows for any MonoBehaviour classes to be globally accessible and guarantee single instantiation
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
+    protected Singleton() {}
+    private static T _instance;
+    
+    public static T Instance
+    {
+        get
+        {
+            if (_instance != null) return _instance;
+            
+            _instance = FindObjectOfType<T>();
+
+            if (_instance == null)
+            {
+                _instance = new GameObject($"({nameof(Singleton<T>)}){typeof(T)}").AddComponent<T>();
+            }
+            
+            return _instance;
+        }
+    }
+    
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        OnAwake();
+    }
+
+    protected virtual void OnAwake() { }
 }
