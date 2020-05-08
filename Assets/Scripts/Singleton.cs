91e08bc2f9e3d794a0b3833345c4a7939fce3e27
Jared Mohney <jared.mohney@gmail.com>
2020-05-08 02:44:14 -0600

    add methods to Singleton.cs

--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -1,10 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-// Singleton take #1
-// Allows for any MonoBehaviour classes to be globally accessible and guarantee single instantiation
-public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+// Allows for any MonoBehaviour classes to be single instantiation and globally accessible
+public abstract class Singleton<T> : Singleton where T : MonoBehaviour
 {
     protected Singleton() {}
     private static T _instance;
@@ -13,14 +13,24 @@ public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
     {
         get
         {
+            if (Quitting) return null;
             if (_instance != null) return _instance;
             
-            _instance = FindObjectOfType<T>();
-
-            if (_instance == null)
+            var instances = FindObjectsOfType<T>();
+            var count = instances.Length;
+            
+            if (count == 1) return _instance = instances[0];
+            if (count >= 2)
             {
-                _instance = new GameObject($"({nameof(Singleton<T>)}){typeof(T)}").AddComponent<T>();
+                _instance = instances[0];
+                Debug.LogWarning($"[{nameof(Singleton<T>)}<{typeof(T)}>] Attempted instantiation of {nameof(Singleton<T>)} of type {typeof(T)} in the scene when {count} were found. Returning first found and destroying others.");
+                instances.ToList().ForEach(Destroy);
+
+                return _instance;
             }
+            
+            Debug.Log($"[{nameof(Singleton<T>)}<{typeof(T)}>] An instance is needed in the scene and none were found, a new instance will be created.");
+            _instance = new GameObject($"({nameof(Singleton<T>)}){typeof(T)}").AddComponent<T>();
             
             return _instance;
         }
@@ -32,5 +42,15 @@ public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
         OnAwake();
     }
 
-    protected virtual void OnAwake() { }
+    protected virtual void OnAwake() {}
+}
+
+public abstract class Singleton : MonoBehaviour
+{
+    protected static bool Quitting { get; private set; }
+    
+    private void OnApplicationQuit()
+    {
+        Quitting = true;
+    }
 }
